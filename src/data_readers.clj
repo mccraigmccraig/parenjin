a0539cbe52150f85b05f5acd1b5a64f0e14dbb30
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2013-11-14 19:46:47 +0000

    add a new data-reader for #app/fix-ref

--- src/data_readers.clj
+++ src/data_readers.clj
@@ -1,2 +1,4 @@
 {application/ref parenjin.application-ref/application-ref-reader
- app/ref         parenjin.application-ref/application-ref-reader}
+ app/ref         parenjin.application-ref/application-ref-reader
+ application/fix-ref parenjin.application-ref/application-fix-ref-reader
+ app/fix-ref parenjin.application-ref/application-fix-ref-reader}
