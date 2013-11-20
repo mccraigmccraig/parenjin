c961b5f1999ec5a35fff71dd126325c9ffe29dd1
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2013-11-20 14:57:40 +0000

    run jobs, in serial or parallel

--- src/parenjin/job.clj
+++ src/parenjin/job.clj
@@ -10,7 +10,7 @@
   (status [this])
   (join [this]))
 
-(defn start-or-noop
+(defn- start-or-noop
   "if it's not running call (defs id) in a future and retain the future in <trackref>"
   [enjin track-ref job-fn]
   (dosync
@@ -20,7 +20,7 @@
                 (future (job-fn enjin))))))
   (util/future-status @track-ref))
 
-(defn stop-or-noop
+(defn- stop-or-noop
   "if it is running, stop the future in (@trackref id)"
   [track-ref]
   (dosync
@@ -28,7 +28,7 @@
      (future-cancel f)))
   (util/future-status @track-ref))
 
-(defn join-or-noop
+(defn- join-or-noop
   "do a blocking deref of a job future, if it's running"
   [track-ref]
   (let [f (-?> track-ref deref deref)]
@@ -44,3 +44,27 @@
 (defn create-job
   [enjin job-fn]
   (map->job {:enjin-promise* enjin (ref nil) job-fn}))
+
+(defn run-jobs-parallel
+  "kick off all jobs in parallel. returns immediately"
+  [& jobs]
+  (->> jobs
+       flatten
+       (map (fn [job] (start job)))
+       doall))
+
+(defn join-jobs
+  "block for the completion of all jobs"
+  [& jobs]
+  (->> jobs
+       flatten
+       (map (fn [job] (join job)))
+       doall))
+
+(defn run-jobs-serial
+  "run all jobs one after another. returns after the last job completes"
+  [& jobs]
+  (->> jobs
+       flatten
+       (map (fn [job] (start job) (join job)))
+       doall))
--- test/parenjin/job_test.clj
+++ test/parenjin/job_test.clj
@@ -1,21 +1,22 @@
 (ns parenjin.job-test
   (:use midje.sweet
         parenjin.job)
-  (:require [parenjin.util :as util])
+  (:require [parenjin.util :as util]
+            [parenjin.job :as job])
   (:import [java.util.concurrent CancellationException]))
 
 (with-state-changes [(around :facts (let [result (promise)
                                           trackref (ref nil)]
                                          ?form ))]
   (fact "start-or-noop should start a never started task"
-    (start-or-noop ..enjin.. trackref (fn [enjin] enjin => ..enjin.. (deliver result ..result..) (Thread/sleep 50))) => :running
+    (#'job/start-or-noop ..enjin.. trackref (fn [enjin] enjin => ..enjin.. (deliver result ..result..) (Thread/sleep 50))) => :running
     (deref result 0 ..fail..) => ..result..))
 
 (with-state-changes [(around :facts (let [result (promise)
                                           trackref (ref (future (Thread/sleep 100) (deliver result ..result..)))]
                                          ?form ))]
   (fact "start-or-noop should do nothing to a running task"
-    (start-or-noop ..enjin.. trackref (fn [enjin] (deliver result ..fail..))) => :running
+    (#'job/start-or-noop ..enjin.. trackref (fn [enjin] (deliver result ..fail..))) => :running
     (deref result 100 ..nocall..) => ..result..))
 
 (with-state-changes [(around :facts (let [b (promise)
@@ -25,30 +26,49 @@
                                          ?form ))]
   (fact "start-or-noop should start a stopped task"
     @b => ..before-result..
-    (start-or-noop ..enjin.. trackref (fn [enjin] enjin => ..enjin.. (deliver a ..after-result..) (Thread/sleep 50))) => :running
+    (#'job/start-or-noop ..enjin.. trackref (fn [enjin] enjin => ..enjin.. (deliver a ..after-result..) (Thread/sleep 50))) => :running
     (deref a 100 ..fail..) => ..after-result..))
 
 (with-state-changes [(around :facts (let [trackref (ref nil)]
                                          ?form ))]
   (fact "stop-or-noop should do nothing to a never started task"
-    (stop-or-noop trackref) => :none))
+    (#'job/stop-or-noop trackref) => :none))
 
 (with-state-changes [(around :facts (let [trackref (ref (future (Thread/sleep 100)))]
                                       ?form ))]
   (fact "stop-or-noop should stop a running task"
-    (stop-or-noop trackref) => :stopped))
+    (#'job/stop-or-noop trackref) => :stopped))
 
 (with-state-changes [(around :facts (let [trackref (ref (future ..result..))]
                                          ?form ))]
   (fact "stop-or-noop should do nothing to a stopped task"
-    (stop-or-noop trackref) => :stopped))
+    (#'job/stop-or-noop trackref) => :stopped))
 
 (with-state-changes [(around :facts (let [trackref (ref (future (Thread/sleep 100) ..result..))]
                                          ?form ))]
   (fact "join-or-noop should deref an existing task's future"
-    (join-or-noop trackref) => :stopped))
+    (#'job/join-or-noop trackref) => :stopped))
 
 (with-state-changes [(around :facts (let [trackref (ref nil)]
                                          ?form ))]
   (fact "join-or-noop should do nothing to a never-started task"
-    (join-or-noop trackref) => :none))
+    (#'job/join-or-noop trackref) => :none))
+
+(fact "run-jobs-parallel should start all jobs in parallel"
+  (run-jobs-parallel [..job1.. ..job2..]) => [..job1-status.. ..job2-status..]
+  (provided
+    (start ..job1..) => ..job1-status..
+    (start ..job2..) => ..job2-status..))
+(fact "join-jobs should block for completion of all jobs"
+  (join-jobs [..job1.. ..job2..]) => [..job1-finished-status.. ..job2-finished-status..]
+  (provided
+    (join ..job1..) => ..job1-finished-status..
+    (join ..job2..) => ..job2-finished-status..))
+
+(fact "run-jobs-serial should run all jobs in sequence"
+  (run-jobs-serial [..job1.. ..job2..]) => [..job1-finished-status.. ..job2-finished-status..]
+  (provided
+    (start ..job1..) => ..job1-status..
+    (join ..job1..) => ..job1-finished-status..
+    (start ..job2..) => ..job2-status..
+    (join ..job2..) => ..job2-finished-status..))
